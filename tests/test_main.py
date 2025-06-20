f5d0c67ee0e1fc9d41c04f55277051825b21cc24
Mariana Carolina Sastre <32931094+planetmariana@users.noreply.github.com>
2025-06-20 17:06:44 +0200

    Cambio en el test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,5 @@ import pytest
 from src.main import get_phrase
 
 def test_get_phrase():
-    expected = "La RECA la construímos entre todos"
+    expected = "La RECA la construímos entre todos, todas y todxs para la comunidad de Python en español"
     assert get_phrase() == expected
