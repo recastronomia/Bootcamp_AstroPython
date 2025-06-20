ceaaa17214dbb60ed14465271e6a5c3328dc7e76
Mariana Carolina Sastre <32931094+planetmariana@users.noreply.github.com>
2025-06-20 16:59:38 +0200

    Mejoras en los docstrings de la funcion

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
 def get_phrase():
     """
     Devuelve una frase que indica que la RECA fue construida entre todos.
+    arguments: None
+    returns: str
+    description: 
+        Esta función es un ejemplo de cómo se puede devolver un mensaje personalizado.
+        Se utiliza para ilustrar la colaboración en el proyecto RECA.
+    example:
+        >>> get_phrase()
+        'La RECA la construímos entre todos'
+    note:
+        Esta función es parte del Bootcamp de AstroPython y se utiliza para enseñar
+        conceptos básicos de Python y colaboración en proyectos.
     """ 
-    return "La RECA la construímos entre todos"
+    return "La RECA la construímos entre todos, todas y todxs para la comunidad de Python en español"
 
 
 if __name__ == "__main__":
