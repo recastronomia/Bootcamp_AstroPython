3a98c75641ffdeff1160c9410f6b5ac87e8772f5
Mariana Carolina Sastre <32931094+planetmariana@users.noreply.github.com>
2025-06-20 17:19:16 +0200

    Incluye los nombres de los estudiantes EN EL PYTEST

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,5 @@ import pytest
 from src.main import get_phrase
 
 def test_get_phrase():
-    expected = "La RECA la construímos entre todos, todas y todxs para la comunidad de Python en español"
+    expected = "La RECA la construímos entre todos, todas y todxs para la comunidad de Python en español, A este bootcamp asistieron Kevin,Diana,Alejandra,Edwin,Alexandra,Catalina,Mariana y Dayana "
     assert get_phrase() == expected
