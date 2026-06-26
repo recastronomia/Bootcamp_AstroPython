5e0c6896cf316db75bb02729edde9f13dfbaca69
Mariana Carolina Sastre <32931094+planetmariana@users.noreply.github.com>
2026-06-26 17:36:45 +0200

    nombre de los asistentes

--- src/main.py
+++ src/main.py
@@ -16,6 +16,42 @@ def get_phrase():
     return "La RECA la construímos entre todos, todas y todxs para la comunidad de Python en español "
 
 
+def get_numbers_1_to_10():
+    """
+    Retorna una lista con los números del 1 al 10.
+    arguments: None
+    returns: list[int]
+    example:
+        >>> get_numbers_1_to_10()
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    return list(range(1, 11))
+
+
+def get_attendees():
+    """
+    Retorna una lista con los nombres de los asistentes al bootcamp.
+    arguments: None
+    returns: list[str]
+    example:
+        >>> get_attendees()
+        ['Steve', 'Jurianny', 'Valentina', 'Juan Carlos', 'Andres', 'Ricardo', 'Luciana', 'Juliana', 'Jean', 'Andres', 'Maria']
+    """
+    return [
+        "Steve",
+        "Jurianny",
+        "Valentina",
+        "Juan Carlos",
+        "Andres",
+        "Ricardo",
+        "Luciana",
+        "Juliana",
+        "Jean",
+        "Andres",
+        "Maria",
+    ]
+
+
 if __name__ == "__main__":
     result = get_phrase()
     print(result)
